1c1c19f7d1e8b7a9a5f0cdc68c8f55c6986ee1f8
Michael Olivier <lists2@baac.net>
2025-03-11 10:37:05 -0700

    Update build and test configuration for Vercel deployment

--- jest.config.cjs
+++ jest.config.cjs
@@ -20,7 +20,7 @@ const customJestConfig = {
     '<rootDir>/node_modules/',
     '<rootDir>/.next/',
     '<rootDir>/__tests__/api/chat/v1/test-utils.ts',
-    '<rootDir>/__tests__/api/chat/v1/utils/streaming.ts',
+    '<rootDir>/__tests__/api/chat/v1/utils/',
     '<rootDir>/__tests__/api/chat/v1/streaming-test-utils.ts',
   ],
   collectCoverage: true,
